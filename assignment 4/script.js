2f9d8cd8f0724e340d8c5da3ada8d3840aee6a15
Deepti678 <100288185+Deepti678@users.noreply.github.com>
2022-03-21 12:00:24 +0530

    update

---
+++ assignment 4/script.js
@@ -0,0 +1,21 @@
+var names = new Array();
+names[0]="Yaakov";
+names[1]="john";
+names[2]="jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
+for (var i = 0; i < names.length; i++)
+{
+	if(names[i].charAt(0)==='j'|| names[i].charAt(0)==='j')
+	{
+		console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("hello "+names[i])
+	}
+}
